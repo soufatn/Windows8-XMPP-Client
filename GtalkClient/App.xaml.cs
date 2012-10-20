4fc55f16fba1cae326a21ca1da51ca40d80b1ba0
Vianney Thurotte <vianney.thurotte@gmail.com>
2012-10-20 11:50:29 +0200

    Add project to github
    - Connect to gtalk with email and password (Console to see)
    - Possible to rewrite the style application on runtime

---
+++ GtalkClient/App.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace GtalkClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static App Instance;
+        public static String Directory ="../";
+        private String _DefaultStyle = "MyStyle.xaml";
+
+        public App()
+        {
+            Instance = this;
+            Directory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string stringsFile = Path.Combine(Directory, "Styles", _DefaultStyle);
+            LoadStyleDictionaryFromFile(stringsFile);
+        }
+
+        /// <summary>
+        /// This funtion loads a ResourceDictionary from a file at runtime
+        /// </summary>
+        public void LoadStyleDictionaryFromFile(string inFileName)
+        {
+            if (File.Exists(inFileName))
+            {
+                try
+                {
+                    using (var fs = new FileStream(inFileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    {
+                        // Read in ResourceDictionary File
+                        var dic = (ResourceDictionary)XamlReader.Load(fs);
+                        // Clear any previous dictionaries loaded
+                        Resources.MergedDictionaries.Clear();
+                        // Add in newly loaded Resource Dictionary
+                        Resources.MergedDictionaries.Add(dic);
+                    }
+                }
+                catch
+                {
+                }
+            }
+            else {
+                Console.WriteLine("Bad File." + inFileName);
+            }
+        }
+    }
+}
---
+++ GtalkClient/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using agsXMPP;
+using agsXMPP.protocol.iq.roster;
+using agsXMPP.protocol.client;
+
+namespace GtalkClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        XmppClientConnection xmppCon = new XmppClientConnection();
+        private bool connected = false;
+        private string _debug = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Connect(object sender, RoutedEventArgs e)
+        {
+            if (!connected)
+            {
+                Jid jidUser = new Jid(email.Text);
+                xmppCon.Username = jidUser.User;
+                xmppCon.Server = jidUser.Server;
+                xmppCon.Password = password.Password;
+                xmppCon.AutoResolveConnectServer = true;
+                //xmppCon.OnRosterStart += new ObjectHandler(xmppCon_OnRosterStart);
+                //xmppCon.OnRosterEnd += new ObjectHandler(xmppCon_OnRosterEnd);
+                xmppCon.OnRosterItem += new agsXMPP.XmppClientConnection.RosterHandler(OnRosterResult);
+                xmppCon.OnRosterEnd += new ObjectHandler(OnRosterEnd);
+                xmppCon.Open();
+                connected = true;
+            }
+            
+        }
+
+        private void OnRosterResult(object sender, agsXMPP.protocol.iq.roster.RosterItem item)
+        {
+            _debug += item.Jid.User.ToString()+"\n";
+        }
+
+        private void OnRosterEnd(object sender) {
+            Console.WriteLine(_debug);
+        }
+
+        private void cmdSend_Click()
+        {
+            // Send a message
+            agsXMPP.protocol.client.Message msg = new agsXMPP.protocol.client.Message();
+            msg.Type = agsXMPP.protocol.client.MessageType.chat;
+            msg.To = new Jid("");
+            msg.Body = "";
+
+            xmppCon.Send(msg);
+        }
+	
+    }
+}
